2f0fd6d0d582950dc3adafbe3a7c3d839140af65
masmiyen <masmiyen@gmail.com>
2023-03-15 23:58:31 -0700

    why betty checks for comments too

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19,
- *   followed by a newline,
- *   Return : Always
+ * main - Entry point
+ * Print a quote using write function
+ * followed by a newline,
+ *
+ *Return: Always 1
  */
 int main(void)
 {
