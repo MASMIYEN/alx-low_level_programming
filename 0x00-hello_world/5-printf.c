9f2373d3e1b2c6e22bb4f1101e13fea806ad9740
masmiyen <masmiyen@gmail.com>
2023-03-15 22:31:12 -0700

    printf betty makes me go like :rage: :rage: :rage:

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar
+ * but the outcome is piece of art
+ * Return: 0 if exited successfully, non-zero otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
