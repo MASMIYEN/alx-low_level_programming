4607c53b4644a5ef3cf09fbff02dd68d44369127
masmiyen <masmiyen@gmail.com>
2023-03-15 22:08:00 -0700

    betty is like an ex

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * Return: 0 if exited successfuly, non-zero otherwise
  */
-int main(void) {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
