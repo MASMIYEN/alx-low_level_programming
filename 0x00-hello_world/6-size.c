653fbaea918024b7db2ddf2beeda75ece82f8606
masmiyen <masmiyen@gmail.com>
2023-03-15 23:04:27 -0700

    size of various types, And I really hate betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,9 @@
 int main(void)
 {
 	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of short: %d byte(s)\n", sizeof(short));
 	printf("Size of int: %d byte(s)\n", sizeof(int));
-	printf("Size of long: %d byte(s)\n", sizeof(long));
-	printf("Size of long long: %d byte(s)\n", sizeof(long long));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of float: %d bytes(s)\n", sizeof(float));
-	printf("Size of double: %d bytes(s)\n", sizeof(double));
-	printf("Size of long double: %d bytes(s)\n", sizeof(long double));
 	return (0);
 }
