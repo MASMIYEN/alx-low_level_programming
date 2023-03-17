676e420740b3866fd256cab5734b72c1c94dfb30
masmiyen <masmiyen@gmail.com>
2023-03-16 22:47:59 -0700

    can we use printf please

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
