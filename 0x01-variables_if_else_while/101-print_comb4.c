76128806a5500f2b8c1e110af9fe44ec3da40b4d
masmiyen <masmiyen@gmail.com>
2023-03-16 23:52:24 -0700

    210 102 201 or whatever

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - print all possible different combinations of three digits
+ * the three digits must be different
+ * print only the smallest combination of three digits
+ * numbers should be printed in ascending order, with three digits
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int n, m, l;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
