08449a39be50accd50efd193c8647cb836c24b42
masmiyen <masmiyen@gmail.com>
2023-03-20 22:30:38 -0700

    Main file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
