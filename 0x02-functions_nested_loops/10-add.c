9461f7cfadab355f9230622a36f256c33de66d52
masmiyen <masmiyen@gmail.com>
2023-03-21 02:50:40 -0700

    1 + 1 = 2 amarite?

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and return the result
+ * @m: int to be added to n
+ * @n: int to be added to m
+ * Return: sum of m and n
+ */
+int add(int m, int n)
+{
+	int o;
+
+	o = m + n;
+
+	return (o);
+}
