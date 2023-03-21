6b536b6b296907253017f988998d5cc8ddbd984f
masmiyen <masmiyen@gmail.com>
2023-03-21 01:42:36 -0700

    abs lol :muscle:

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @m: the int to be checked
+ * Return: the absolute value of int
+ */
+int _abs(int m)
+{
+	if (m >= 0)
+	{
+		return (m);
+	}
+	return (-m);
+}
