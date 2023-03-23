e15c23aedb1c79cd4dd8778bf771c714f1b685e5
masmiyen <masmiyen@gmail.com>
2023-03-23 06:45:19 -0700

    update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+int _putchar(char c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
