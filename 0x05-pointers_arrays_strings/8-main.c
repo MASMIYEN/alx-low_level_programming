70a6948bf78e483142ab940c04526dc62807701e
masmiyen <masmiyen@gmail.com>
2023-03-27 11:12:06 -0700

    print array

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -10,7 +10,7 @@ int main(void)
 	int array[5];
 
 	array[0] = 98;
-	arrey[1] = 402;
+	array[1] = 402;
 	array[2] = -198;
 	array[3] = 298;
 	array[4] = -1024;
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array
+ * @a: array name
+ * @n: number of elements of the printed array
+ *
+ * Return: a and n inputs
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
+}
