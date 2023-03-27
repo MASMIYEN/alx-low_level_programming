890aa5e26457378e94badb8e92575070ad6325ca
masmiyen <masmiyen@gmail.com>
2023-03-27 10:28:24 -0700

    esrever ni gnirts a tnirp

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ * Return: 0.
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
