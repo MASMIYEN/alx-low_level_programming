9957e49a2cb4f34ea33cdb146a0337a22c43a153
masmiyen <masmiyen@gmail.com>
2023-03-30 05:04:40 -0700

    stray cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int m;
+	int n;
+
+	m = 0;
+	while (dest[m] != '\0')
+	{
+		m++;
+	}
+	n = 0;
+	while (src[j] != '\0')
+	{
+		dest[m] = src[n];
+		m++;
+		n++;
+	}
+
+	dest[m] = '\0';
+	return (dest);
+}
