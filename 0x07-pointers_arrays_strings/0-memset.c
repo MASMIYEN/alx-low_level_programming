5223bd9a3b3f129aeb3f90f21c7ef45ab9ee9ead
masmiyen <masmiyen@gmail.com>
2023-04-03 12:58:34 -0700

    why betty telling me there no description for memset function while its right there

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /*
- * _memset - a function that fills memory with a constant byte.
+ * _memset - a function that fills memory with a constant byte
+ *
  * @s: pointer char type
- * @b: variable of the char type
  * @n: unsigned int variable to be filled
  * @m: unsigned int so the value will always be positive or zero
  *
- * Return: pointer to filled memory 
+ * Return: pointer to filled memory
  */
 
 char *_memset(char *s, char b, unsigned int n)
