d5649445d1c118f4ae918d6e2387350842bc49a7
masmiyen <masmiyen@gmail.com>
2023-04-03 13:06:55 -0700

    If betty has million haters, then I'm one of them.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
-/*
+/**
  * _memset - a function that fills memory with a constant byte
  *
  * @s: pointer char type
  * @n: unsigned int variable to be filled
- * @m: unsigned int so the value will always be positive or zero
+ * @b: char type
  *
  * Return: pointer to filled memory
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	/**
+	 * unsigned int so the value will be positive or zero
+	 */
 	unsigned int m;
 
 	for (m = 0; m < n; m++)
