3cd88ec3c285a2de3029d6086cb461365bd631e3
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-04-07 23:07:14 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,20 +4,20 @@
  * @s1: input value
  * @s2: input value
  *
- * Return: s1[i] - s2[i]
+ * Return: s1[m] - s2[m]
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int m;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	m = 0;
+	while (s1[m] != '\0' && s2[m] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[m] != s2[m])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[m] - s2[m]);
 		}
-		i++;
+		m++;
 	}
 	return (0);
 }
