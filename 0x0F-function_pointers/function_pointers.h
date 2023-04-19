05d5f8bdc2b8b13117d8b46e9bb008678f8a7d46
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-04-19 08:42:07 +0000

    main files

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCT_H
+#define FUNCT_H
+#include <stdlib.h> 
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
