06dae68c501550de64ab75687447bcc3aa9a4f1c
masmiyen <masmiyen@gmail.com>
2023-04-20 08:33:23 -0700

    header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARDIADIC_FUNCTIONS
+#define VARDIADIC_FUNCTIONS
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
