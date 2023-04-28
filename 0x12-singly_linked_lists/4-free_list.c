b6fcc539c213d91e85df38082b2c2f757f248da9
masmiyen <masmiyen@gmail.com>
2023-04-27 17:45:03 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free list 
+ *
+ * @head: pointer of the firts node
+ *
+ * Return: absolutly nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *actual; /* declaring */
+
+	while ((actual = head) != NULL)
+	{
+		head = head->next;
+		free(actual->str);
+		free(actual);
+	}
+}
