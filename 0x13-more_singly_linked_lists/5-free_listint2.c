8674439cbeefd4dcf1304e69f8272fcf33c2ab55
masmiyen <masmiyen@gmail.com>
2023-04-29 10:48:46 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - freedom for list 
+ *
+ * @head: pointer of head node
+ *
+ * Return: again its a void function duuh
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *act, *rid;
+
+	if (head != NULL)
+	{
+		act = *head;
+
+		while ((rid = act) != NULL)
+		{
+			act = act->next;
+
+			*head = NULL;
+		}
+		free(rid);
+	}
+}
