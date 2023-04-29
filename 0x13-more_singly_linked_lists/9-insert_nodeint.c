b7dfdc95dd846774653e8ca7d488363f1aa7789d
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-04-29 23:39:17 +0100

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,41 +12,40 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int idex;
-	listint_t nv_node, ac_node;
+	listint_t *nv_node, *ac_node;
 
-	ac_node = *head;
+	if (head == NULL)
+		return (NULL);
 
 	nv_node = malloc(sizeof(listint_t));
-
-	if (idex != 0 && *head == NULL && (nv_node == NUL))
+	if (nv_node == NULL)
 		return (NULL);
 
 	nv_node->n = n;
+	nv_node->next = NULL;
 
-	for (idex = 0; head != NULL && idex < idx - 1; idex++)
-	{
-		ac_node = ac_node->next;
-		if (ac_node == NULL)
-			return (NULL);
-	}
+	ac_node = *head;
 
-	if (ac_node->next)
-	{
-		nv_node->next = ac_node->next;
-		ac_node->next = nv_node;
-	}
-
-	else if (idx == 0)
+	if (idx == 0)
 	{
 		nv_node->next = *head;
 		*head = nv_node;
+		return (nv_node);
 	}
 
-	else
+	for (idex = 0; ac_node != NULL && idex < idx - 1; idex++)
 	{
-		nv_node->next = NULL;
-		ac_node->next = nv_node;
+		ac_node = ac_node->next;
 	}
+
+	if (ac_node == NULL)
+	{
+		free(nv_node);
+		return (NULL);
+	}
+
+	nv_node->next = ac_node->next;
+	ac_node->next = nv_node;
 
 	return (nv_node);
 }
