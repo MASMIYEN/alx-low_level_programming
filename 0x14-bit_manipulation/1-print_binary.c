2eadbcda90f9489ccd83d4cc33aa5be20b701f9e
masmiyen <masmiyen@gmail.com>
2023-05-04 13:33:30 -0700

    1. 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary represtentation 
+ * of a numberr
+ *
+ * @n: parameter of the function
+ *
+ * Return: binary rep
+ */
+
+void print_binary(unsigned long int n)
+{
+	int m;
+	for (m = 63; m >= 0;m--)
+	{
+		printf("%ld", (n >> m) & 1);
+	}
+	printf("\n");
+}
