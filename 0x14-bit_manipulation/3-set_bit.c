cb8088c86aa2e446786ac90aebf3856527c31921
masmiyen <masmiyen@gmail.com>
2023-05-04 13:53:00 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,6 +13,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(unsigned long int) *8 - 1))
 		return (-1);
 
-	*n |= (iul << index);
+	*n |= (1UL << index);
 	return (1);
 }
