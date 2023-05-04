7ac3d3728b3dbcac42eba6923eac59fef00f5cf0
masmiyen <masmiyen@gmail.com>
2023-05-04 15:02:54 -0700

    find whitespace

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** 
+ * get_endianness - check endians
+ *
+ * Return: 0 big endian || 1 litlle endian
+ */
+int get_endianness(void)
+{
+	unsigned int m = 0x01;
+	char *ch = (char *)&m;
+
+	return ((int)*ch);
+}
