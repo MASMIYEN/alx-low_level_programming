4997cf7b481b90689c304175dad66de1e80256ae
masmiyen <masmiyen@gmail.com>
2023-05-08 12:12:37 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,26 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int file_des, res, len;
+	int file_des, res, len;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_des == -1)
+		return (-1);
 
-    file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (file_des == -1)
-        return (-1);
-
-    if (text_content != NULL)
-    {
-        len = strlen(text_content);
-        res = write(file_des, text_content, len);
-        if (res == -1 || res != len)
-        {
-            close(file_des);
-            return (-1);
-        }
-    }
-
-    close(file_des);
-    return (1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		res = write(file_des, text_content, len);
+		if (res == -1 || res != len)
+		{
+			close(file_des);
+			return (-1);
+		}
+	}
+	close(file_des);
+	return (1);
 }
