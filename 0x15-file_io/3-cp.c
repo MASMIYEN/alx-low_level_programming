dfbc4511785103ac2ef11b663ee74af10041d626
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-05-09 00:25:07 +0100

    40lines

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,8 +29,8 @@ int main(int argc, char *argv[])
 		error("Usage: %s file_from file_to\n", argv[0], 97);
 	}
 
-	int cp_from = open(argv[1], O_RDONLY);
-	int cp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	int cp_from = open(argv[1], O_RDONLY),
+		cp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
 	if (cp_from == -1)
 	{
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error(char *log_msg, char *arg, int code);
 
 #endif
