79bbab0d1b81172f6fdf52af39656fe4540e232d
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-05-08 18:08:00 +0100

    found white space

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * ssize_t read_textfile - reads a text file and prints it 
+ * ssize_t read_textfile - reads a text file and prints it
  * to the POSIX standard output
  *
  * @filemame: file name
