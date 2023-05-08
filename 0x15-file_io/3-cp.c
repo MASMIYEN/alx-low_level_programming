6dcc792ba65ca26f51770b70f8b24f345e0d8f29
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-05-09 00:41:25 +0100

    40lines

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,69 +1,85 @@
 #include "main.h"
 
+#define MAXSIZE 1024
+
+
 /**
- * error - handle error
+ * _error - prints error messages and exits with exit number
  *
- * @log_msg: log message displayed
- * @arg: argument passed to program
- * @code: message code
+ * @error: either the exit number or file descriptor
+ * @str: name of either file_in or file_out
+ * @file_d: file descriptor
  *
- * Return: code
- */
-void error(char *log_msg, char *arg, int code)
+ * Return: 0 on success
+*/
+int _error(int error, char *str, int file_d)
 {
-	dprintf(STDERR_FILENO, log_msg, arg);
-	exit(code);
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+			exit(error);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+			exit(error);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close file_d %d\n", file_d);
+			exit(error);
+		default:
+			return (0);
+	}
 }
+
 /**
- * main - copies the content of a file to another file
+ * main - create a copyfile
  *
- * @argc: arg count
- * @argv:string's array
+ * @argc: argument count
+ * @argv: argument passed by
  *
  * Return: 0
- */
+*/
 int main(int argc, char *argv[])
 {
+	int file_in, file_out;
+	int read_stat, write_stat;
+	int close_in, close_out;
+	char buffer[MAXSIZE];
+
+
 	if (argc != 3)
+		_error(97, NULL, 0);
+
+
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+		_error(98, argv[1], 0);
+
+
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		_error(99, argv[2], 0);
+
+
+	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
 	{
-		error("Usage: %s file_from file_to\n", argv[0], 97);
+		if (read_stat == -1)
+			_error(98, argv[1], 0);
+
+		write_stat = write(file_out, buffer, read_stat);
+		if (write_stat == -1)
+			_error(99, argv[2], 0);
 	}
 
-	int cp_from = open(argv[1], O_RDONLY),
-		cp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	close_in = close(file_in);
+	if (close_in == -1)
+		_error(100, NULL, file_in);
 
-	if (cp_from == -1)
-	{
-		error("Error: Can't read from file %s\n", argv[1], 98);
-	}
-	if (cp_to == -1)
-	{
-		error("Error: Can't write to %s\n", argv[2], 99);
-	}
+	close_out = close(file_out);
+	if (close_out == -1)
+		_error(100, NULL, file_out);
 
-	char buffer[BUFFER_SZ];
-	ssize_t bts_read, bts_written;
-
-	bts_written = write(cp_to, buffer, bts_read);
-
-	while ((bts_read = read(cp_from, buffer, BUFFER_SZ)) > 0)
-	{
-		if (bts_written != bts_read)
-		{
-			error("Error: Write to %s failed\n", argv[2], 99);
-		}
-	}
-	if (bts_read == -1)
-	{
-		error("Error: Can't read from file %s\n", argv[1], 98);
-	}
-	if (close(cp_from) == -1)
-	{
-		error("Error: Can't close fd %d\n", cp_from, 100);
-	}
-	if (close(cp_to) == -1)
-	{
-		error("Error: Can't close fd %d\n", cp_to, 100);
-	}
 	return (0);
 }
