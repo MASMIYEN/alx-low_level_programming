8651c362a14f4a41399f54e7da5ee06acb4181d5
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-05-08 18:43:40 +0100

    code fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,23 @@
  *
  * Return: 1 on success || -1 on failure
 */
+
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return (-1);
-
     int file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file_des == -1)
+    ssize_t w = write(file_des, text_content, len);
+    int len = 0;
+
+    if (filename == NULL || file_des == -1)
         return (-1);
 
-    if (text_content != NULL) {
-        int len = 0;
+    if (text_content != NULL)
+    {
         while (text_content[len] != '\0')
             len++;
-        ssize_t w = write(file_des, text_content, len);
-        if (w == -1) {
+
+        if (w == -1)
+        {
             close(file_des);
             return (-1);
         }
