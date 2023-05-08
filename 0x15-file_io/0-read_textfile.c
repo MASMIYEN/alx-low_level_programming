2c1f1cbae9c16da5a17141a879a78b53df3cb2a0
masmiyen <masmiyen@gmail.com>
2023-05-08 10:06:22 -0700

    find whitespace

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * ssize_t read_textfile - reads a text file and prints it 
+ * to the POSIX standard output
+ *
+ * @filemame: file name
+ * @letters: letters to print
+ *
+ * Return: 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+
+	FILE *fl = fopen(filename, "r");
+	if (fl == NULL)
+		return (0);
+
+	char *buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		fclose(fl);
+		return (0);
+	}
+	ssize_t bytes_read = fread(buffer, sizeof(char), letters, fl);
+	if (bytes_read == 0)
+	{
+		fclose(fl);
+		free(buffer);
+		return (0);
+	}
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		fclose(fl);
+		free(buffer);
+		return (0);
+	}
+	fclose(fl);
+	free(buffer);
+	return (bytes_written);
+}
