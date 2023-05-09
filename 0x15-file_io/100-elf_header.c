09bea8f76d8a68a343c0c7a30bc087ca98b3f7f7
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-05-09 02:15:28 +0100

    pleeease

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,40 +11,54 @@
  * Return: 0
  */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: elf_header elf_filename\n");
-    exit(98);
-  }
-  FILE * file = fopen(argv[1], "rb");
-  if (file == NULL) {
-    perror("fopen");
-    exit(98);
-  }
-  if (fseek(file, 0, SEEK_SET) != 0) {
-    perror("fseek");
-    exit(98);
-  }
-  struct elf_header header;
-  if (fread( & header, sizeof(header), 1, file) != 1) {
-    perror("fread");
-    exit(98);
-  }
-  if (header.e_ident[0] != ELF_MAGIC) {
-    fprintf(stderr, "Not an ELF file\n");
-    exit(98);
-  }
-  printf("Magic: 0x%08x\n", header.e_ident[0] | (header.e_ident[1] << 8) | (header.e_ident[2] << 16) | (header.e_ident[3] << 24));
-  printf("Class: %d\n", header.e_ident[4]);
-  printf("Data: %d\n", header.e_ident[5]);
-  printf("Version: %d\n", header.e_ident[6] | (header.e_ident[7] << 8));
-  printf("OS/ABI: %d\n", header.e_ident[8]);
-  printf("ABI Version: %d\n", header.e_ident[9] | (header.e_ident[10] << 8));
-  printf("Type: %d\n", header.e_type);
-  printf("Entry point address: 0x%08x\n", header.e_entry);
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(98);
+	}
 
-  fclose(file);
+	FILE *fp = fopen(argv[1], "rb");
+	if (fp == NULL)
+	{
+		perror("fopen");
+		exit(98);
+	}
 
-  return (0);
+	// Seek to the beginning of the ELF header.
+	if (lseek(fileno(fp), 0, SEEK_SET) != 0)
+	{
+		perror("lseek");
+		exit(98);
+	}
+
+	// Read the ELF header.
+	struct elf_header header;
+	if (fread(&header, sizeof(header), 1, fp) != 1)
+	{
+		perror("fread");
+		exit(98);
+	}
+
+	// Check if the file is an ELF file.
+	if (header.e_ident[0] != ELF_MAGIC)
+	{
+		fprintf(stderr, "Not an ELF file.\n");
+		exit(98);
+	}
+
+	// Print the information contained in the ELF header.
+	printf("Magic: 0x%08x\n", header.e_ident[1] << 24 | header.e_ident[2] << 16 | header.e_ident[3] << 8 | header.e_ident[4]);
+	printf("Class: %d\n", header.e_ident[5]);
+	printf("Data: %d\n", header.e_ident[6]);
+	printf("Version: %d\n", header.e_version);
+	printf("OS/ABI: %s\n", header.e_osabi);
+	printf("ABI Version: %d\n", header.e_abiversion);
+	printf("Type: %d\n", header.e_type);
+	printf("Entry point address: 0x%08x\n", header.e_entry);
+
+	fclose(fp);
+
+	return 0;
 }
