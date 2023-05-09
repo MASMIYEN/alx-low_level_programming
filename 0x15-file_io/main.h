642702ab1725717d5b24f8191a73465fb2c4871a
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-05-09 02:48:45 +0100

    pleeease

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,24 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
+#define ELF_MAGIC 0x7F454C46
+struct elf_header
+{
+	unsigned char e_ident[16];
+	unsigned short e_type;
+	unsigned short e_machine;
+	unsigned int e_version;
+	unsigned long e_entry;
+	unsigned long e_phoff;
+	unsigned long e_shoff;
+	unsigned int e_flags;
+	unsigned short e_ehsize;
+	unsigned short e_phentsize;
+	unsigned short e_phnum;
+	unsigned short e_shentsize;
+	unsigned short e_shnum;
+	unsigned short e_shstrndx;
+};
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
