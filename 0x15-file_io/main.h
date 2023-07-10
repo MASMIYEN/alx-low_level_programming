2e3d49844cf572293581d1747c83347d0f88e8f6
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-07-10 07:04:02 +0100

    file I/O

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,37 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <elf.h>
+#define ELF_MAGIC 0x7F454C46
+struct elf_header
+{
+	unsigned char e_ident[16];
+	unsigned short e_type;
+	unsigned short e_machine;
+	unsigned int e_version;
+	unsigned long e_entry;
+	unsigned long e_phoff;
+	unsigned long e_shoff;
+	unsigned int e_flags;
+	unsigned short e_ehsize;
+	unsigned short e_phentsize;
+	unsigned short e_phnum;
+	unsigned short e_shentsize;
+	unsigned short e_shnum;
+	unsigned short e_shstrndx;
+};
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void print_error_and_exit(int error, char *filename, int fd);
+
+#endif
