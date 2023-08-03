87d380fef31025a9e5be1b6518174ac53004c758
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-03 14:29:21 +0100

    print binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary represtentation
+ * of a numberr
+ *
+ * @n: parameter of the function
+ *
+ * Return: binary rep
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) ? '1' : '0');
+}
