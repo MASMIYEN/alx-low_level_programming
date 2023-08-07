e44d09c00f4e88d97fc5dfeb189c0ac6ccb899bb
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-07 06:54:41 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ *
+ * @filename: the name of the file
+ * @text_content: the content of the file
+ *
+ * Return: 1 on success || -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_des, res, len;
+
+	if (filename == NULL)
+		return (-1);
+	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_des == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		res = write(file_des, text_content, len);
+		if (res == -1 || res != len)
+		{
+			close(file_des);
+			return (-1);
+		}
+	}
+	close(file_des);
+	return (1);
+}
