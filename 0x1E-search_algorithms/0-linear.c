bcdafac4c457314d44865533ec36eb8becbd40d0
MASMIYEN <masmiyen@gmail.com>
2024-01-16 06:39:38 +0100

    trying betty fixes

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * linear_search - function
@@ -12,7 +11,7 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
