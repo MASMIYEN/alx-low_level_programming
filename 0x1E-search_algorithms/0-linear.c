cd63482f20b337b5318f8d9cfbb2a0b64bfd8c91
MASMIYEN <masmiyen@gmail.com>
2024-01-16 02:15:01 +0100

    1. Binary search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,23 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * linear_search - function
- * @array: desc
- * @size: desc
- * @value: desc
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
  *
- * Return: Return
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+    int i;
 
-    for (i = 0; i < size; i++)
+    if (array == NULL)
+        return (-1);
+
+    for (i = 0; i < (int)size; i++)
     {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
+        printf("Value checked array[%u] = [%d]\n", i, array[i]);
+        if (value == array[i])
             return (i);
     }
     return (-1);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
